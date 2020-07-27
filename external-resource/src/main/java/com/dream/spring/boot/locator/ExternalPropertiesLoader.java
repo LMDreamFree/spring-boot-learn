3e659f3026207138cd6348cafa9a5ed37a90f9a1
limeng <987568224@qq.com>
2020-07-27 10:15:33 +0800

    BeanPostProcessor

--- external-resource/src/main/java/com/dream/spring/boot/locator/ExternalPropertiesLoader.java
+++ external-resource/src/main/java/com/dream/spring/boot/locator/ExternalPropertiesLoader.java
@@ -1,7 +1,6 @@
 package com.dream.spring.boot.locator;
 
 import com.dream.spring.boot.source.ExternalProperties;
-import org.springframework.beans.MutablePropertyValues;
 import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.boot.env.PropertiesPropertySourceLoader;
 import org.springframework.cloud.bootstrap.config.PropertySourceLocator;
@@ -9,11 +8,12 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.env.CompositePropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.env.PropertySource;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.DefaultPropertySourceFactory;
 import org.springframework.core.io.support.EncodedResource;
+import org.springframework.core.io.support.PropertySourceFactory;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -31,6 +31,10 @@ import java.util.*;
 @Order(Ordered.HIGHEST_PRECEDENCE + 100)
 public class ExternalPropertiesLoader implements PropertySourceLocator {
 
+    public final PropertySourceFactory DEFAULT_PROPERTY_SOURCE_FACTORY = new DefaultPropertySourceFactory();
+
+    public final DefaultResourceLoader RESOURCE_LOADER = new DefaultResourceLoader();
+
     private final ExternalProperties externalProperties;
 
     public ExternalPropertiesLoader(ExternalProperties externalProperties) {
@@ -43,24 +47,26 @@ public class ExternalPropertiesLoader implements PropertySourceLocator {
                 "EXTERNAL_SOURCE");
         externalProperties.setEnvironment(environment);
         String fileName = environment.getProperty("source.fileName");
-        Properties properties = loadProperty(fileName);
-        Map<String, Object> map = propertiesToMap(properties);
-        MapPropertySource mapPropertySource = new MapPropertySource("external_map", map);
-        composite.addFirstPropertySource(mapPropertySource);
+        PropertySource<?> properties = loadProperties(fileName);
+       // MapPropertySource mapPropertySource = new MapPropertySource("external_map", properties);
+        composite.addFirstPropertySource(properties);
         return composite;
     }
 
-    private Properties loadProperty(String sourceName) {
-        DefaultResourceLoader resourceLoader = new DefaultResourceLoader();
-        Resource resource = resourceLoader.getResource(sourceName);
+    private PropertySource<?> loadProperties(String sourceName) {
+
+        Resource resource = RESOURCE_LOADER.getResource(sourceName);
+
         PropertiesPropertySourceLoader loader = new PropertiesPropertySourceLoader();
         try {
-            List<PropertySource<?>> load = loader.load(sourceName, resource);
-            PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
             EncodedResource encodedResource = new EncodedResource(resource, StandardCharsets.UTF_8);
+            List<PropertySource<?>> load = loader.load(sourceName, encodedResource.getResource());
+            PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
+            PropertySource<?> propertySource = DEFAULT_PROPERTY_SOURCE_FACTORY.createPropertySource(sourceName, encodedResource);
             propertiesFactoryBean.setLocation(encodedResource.getResource());
             propertiesFactoryBean.afterPropertiesSet();
-            return propertiesFactoryBean.getObject();
+            propertiesFactoryBean.getObject();
+            return propertySource;
         } catch (IOException e) {
             e.printStackTrace();
         }
